ec3e36abe2de97e2ce4b1071f32d95801c67896a
achmedhibatillah <ach.hibatillah@gmail.com>
2025-04-14 23:23:48 +0700

    Updated!

--- app/Models/Cuti.php
+++ app/Models/Cuti.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cuti extends Model
 {
-    //
+    protected $table = 'cuti';
+    protected $primaryKey = 'cuti_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'cuti_id',
+        'cuti_status',
+        'cuti_mulai',
+        'cuti_selesai',
+        'cuti_alasan',
+        'user_id',
+    ];
 }
