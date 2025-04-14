d6fe39177d03a1c1b2e0277bf917cecb0b4baee0
achmedhibatillah <ach.hibatillah@gmail.com>
2025-04-14 23:21:23 +0700

    Updated!

---
+++ database/migrations/2025_04_14_231734_cuti.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cuti', function(Blueprint $table) {
+            $table->string('cuti_id', 35)->primary();
+            $table->integer('cuti_status');
+            $table->date('cuti_mulai');
+            $table->date('cuti_selesai');
+            $table->string('cuti_alasan', 350)->nullable();
+            $table->string('user_id', 35);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('cuti');
+    }
+};
