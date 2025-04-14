6f9361084996e2138e3f213352edcd52a685a58d
achmedhibatillah <ach.hibatillah@gmail.com>
2025-04-14 12:57:43 +0700

    Updated!

--- app/Models/Lembur.php
+++ app/Models/Lembur.php
@@ -28,32 +28,32 @@ class Lembur extends Model
             ->paginate(10);
     
 
-            $paginator->getCollection()->transform(function ($lembur) {
-                $mulai = Carbon::createFromFormat('H:i:s', $lembur->lembur_mulai);
-                $selesai = Carbon::createFromFormat('H:i:s', $lembur->lembur_selesai);
-                $durasiMenit = $mulai->diffInMinutes($selesai);
-                $jam = floor($durasiMenit / 60);
-                $menit = $durasiMenit % 60;
-                $lembur_durasi = ($jam > 0 ? "{$jam} jam " : '') . ($menit > 0 ? "{$menit} menit" : '');
+        $paginator->getCollection()->transform(function ($lembur) {
+            $mulai = Carbon::createFromFormat('H:i:s', $lembur->lembur_mulai);
+            $selesai = Carbon::createFromFormat('H:i:s', $lembur->lembur_selesai);
+            $durasiMenit = $mulai->diffInMinutes($selesai);
+            $jam = floor($durasiMenit / 60);
+            $menit = $durasiMenit % 60;
+            $lembur_durasi = ($jam > 0 ? "{$jam} jam " : '') . ($menit > 0 ? "{$menit} menit" : '');
 
-                if ($lembur->lembur_status == 0 && $lembur->lembur_tgl <= now()) {
-                    $lembur_status = 'Kadaluarsa';
-                } else if ($lembur->lembur_status == 0 && $lembur->lembur_tgl > now()) {
-                    $lembur_status = 'Menunggu konfirmasi';
-                }
-            
-                return [
-                    'lembur_id' => $lembur->lembur_id,
-                    'lembur_tgl' => $lembur->lembur_tgl,
-                    'lembur_mulai' => $lembur->lembur_mulai,
-                    'lembur_selesai' => $lembur->lembur_selesai,
-                    'lembur_durasi' => $lembur_durasi,
-                    'lembur_catatan' => $lembur->lembur_catatan,
-                    'lembur_status' => $lembur_status,
-                    'created_at' => $lembur->created_at,
-                    'updated_at' => $lembur->updated_at,
-                ];
-            });
+            if ($lembur->lembur_status == 0 && $lembur->lembur_tgl <= now()) {
+                $lembur_status = 'Kadaluarsa';
+            } else if ($lembur->lembur_status == 0 && $lembur->lembur_tgl > now()) {
+                $lembur_status = 'Menunggu konfirmasi';
+            }
+        
+            return [
+                'lembur_id' => $lembur->lembur_id,
+                'lembur_tgl' => Carbon::parse($lembur->lembur_tgl)->translatedFormat('l, d F Y'),
+                'lembur_mulai' => $lembur->lembur_mulai,
+                'lembur_selesai' => $lembur->lembur_selesai,
+                'lembur_durasi' => $lembur_durasi,
+                'lembur_catatan' => $lembur->lembur_catatan,
+                'lembur_status' => $lembur_status,
+                'created_at' => $lembur->created_at,
+                'updated_at' => $lembur->updated_at,
+            ];
+        });
             
     
         return $paginator;
