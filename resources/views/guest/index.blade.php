358de47bf9afc7233e445291620b80f4d6ba8341
achmedhibatillah <ach.hibatillah@gmail.com>
2025-04-23 12:32:59 +0700

    Updated!

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,1 +1,8 @@
-
+<div class="d-flex justify-content-center align-items-center" style="min-height:100vh;">
+    <div class="" style="margin:100px 0;">
+        <img src="{{ asset('assets/images/static/elements/logo-text.png') }}" alt="Bumi Suksesindo" style="width:180px;">
+        <div class="mt-3 d-flex gap-2">
+            <a href="{{ url('login') }}" class="btn btn-clr2 btn-sm">Login <i class="fas fa-sign-in"></i></a>
+        </div>
+    </div>
+</div>
